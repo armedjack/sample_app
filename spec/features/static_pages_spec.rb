5adc72f466137830e2d8cd8f1e2a961d82582f6a
Armedjak <armedjack@gmail.com>
2013-10-31 11:15:14 +0800

    Make proper test and refactor

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -2,46 +2,64 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
-  subject {page}
+  subject { page }
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    should have_selector('h1', text: "About")
+    click_link "Help"
+    should have_selector('h1', text: "Help")
+    click_link "Contact us"
+    should have_selector('h1', text: "Contact us")
+    click_link "Home"
+    click_link "Регистрируйтесь!"
+    should have_selector('h1', text: "Sign up")
+    click_link "sample app"
+    should have_selector('h1', text: "Sign up")
+  end
+
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    
+  end
 
   describe "Contacts page" do
     before { visit contacts_path }
 
-    it { should have_selector('h1', text: 'Contact us') }
-    it { should have_title(full_title('Contacts')) }    
+    let(:heading) { 'Contact us' }
+    let(:page_title) { 'Contacts' }  
+    it_should_behave_like "all static pages" 
 
   end
 
 	describe "Home page" do
     before { visit root_path }
 
-    it { should have_selector('h1', text: 'Приветствуем в Sample App') }
-    it { should have_title(full_title('')) }
-    it { should_not have_title(full_title('Home')) }	
+    let(:heading) { 'Приветствуем в Sample App' }
+    let(:page_title) { '' } 
+    it_should_behave_like "all static pages"
     
 	end
 
   describe "Help page" do
-   before { visit help_path }
+    before { visit help_path }
 
-   it { should have_selector('h1', text: 'Help') }
-   it { should have_title(full_title('Help')) }  
+    let(:heading) { 'Help' }
+    let(:page_title) { 'Help' }  
+    it_should_behave_like "all static pages"
 
   end
 
   describe "About page" do
     before { visit about_path }
 
-    it { should have_selector('h1', text: 'About') }
-    it { should have_title('About') }
+    let(:heading) { 'About' }
+    let(:page_title) { 'About' }  
+    it_should_behave_like "all static pages"
   end
 
 
-  # describe "GET /static_pages" do
-  #   it "works! (now write some real specs)" do
-  #     # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-  #     get static_pages_index_path
-  #     response.status.should be(200)
-  #   end
-  # end
+
+
 end
--- spec/support/utilities.rb
+++
@@ -1,7 +0,0 @@
-def full_title(page_title)
-
-  base_title = "My sample App"
-  page_title.empty? ? base_title : "#{base_title} | #{page_title}"
-
-end
-
