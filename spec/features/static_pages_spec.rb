51378e638ae8580aa0d839ab1ff8f08524525c4a
armedjack <armedjack@gmail.com>
2013-11-07 19:11:46 +0700

    Initial commit on Ubuntu! Yey!

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -16,19 +16,18 @@ describe "StaticPages" do
     click_link "Регистрируйтесь!"
     should have_selector('h1', text: "Sign up")
     click_link "sample app"
-    should have_selector('h1', text: "Sign up")
+    should have_selector('h1', text: "Приветствуем в Sample App")
   end
 
   shared_examples_for "all static pages" do
     it { should have_selector('h1', text: heading) }
-    
+      
   end
 
   describe "Contacts page" do
     before { visit contacts_path }
 
-    let(:heading) { 'Contact us' }
-    let(:page_title) { 'Contacts' }  
+    let(:heading) { 'Contact us' }    
     it_should_behave_like "all static pages" 
 
   end
@@ -36,8 +35,7 @@ describe "StaticPages" do
 	describe "Home page" do
     before { visit root_path }
 
-    let(:heading) { 'Приветствуем в Sample App' }
-    let(:page_title) { '' } 
+    let(:heading) { 'Приветствуем в Sample App' }    
     it_should_behave_like "all static pages"
     
 	end
@@ -46,7 +44,6 @@ describe "StaticPages" do
     before { visit help_path }
 
     let(:heading) { 'Help' }
-    let(:page_title) { 'Help' }  
     it_should_behave_like "all static pages"
 
   end
@@ -55,11 +52,7 @@ describe "StaticPages" do
     before { visit about_path }
 
     let(:heading) { 'About' }
-    let(:page_title) { 'About' }  
     it_should_behave_like "all static pages"
   end
-
-
-
 
 end
